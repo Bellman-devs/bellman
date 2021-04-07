cebb9e7a6158e72ffd1ee9be9d2e54da99fb887b
hstojic <hrvoje.stojic@protonmail.com>
2021-04-07 14:25:52 +0100

    corrected workflows; small updates to the readme; corrected docs version

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, The Bellman Contributors"
 author = "The Bellman Contributors"
 
 # The full version, including alpha/beta/rc tags
-release = "1.0"
+release = "0.1.0"
 
 # -- General configuration ---------------------------------------------------
 
